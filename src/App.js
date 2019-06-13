351a2190384bef5b39e4c2e0ed678f8d9c296784
mehdigoom <medhi.gay@sfr.fr>
2019-06-13 11:05:08 +0200

    etape 1 prise en coup des states

--- src/App.js
+++ src/App.js
@@ -16,25 +16,45 @@ class App extends React.Component {
 titre : "Bienvenue Dans le support Dood",
 message : "j'ai besoin d'aide !",
 prossse : false,
-hidenhome:""
+step:0
+
 
     };
-
-    
+  
+ 
+  
   };
  
+
+  handleClick() {
+    if(this.state.prossse){
+      this.setState({
+        message : "retour",
+    prossse: false,
+    step :0
+    })
+    }else{
+      this.setState({
+        message : "j'ai besoin d'aide !",
+      prossse: true,
+      step:1
+    })
+    }  
+  }
+
+
+
+
   render() {
-    
+   
     return (
     <div className="App">
       <header className="App-header">
    <h1>{this.state.titre}</h1>
-   <button>{this.state.message}</button>
+   <button onClick={this.handleClick.bind(this)}>{this.state.message}</button>
 
       </header>
-      <body>
-      
-      </body>
+     
    
     </div>
      );
