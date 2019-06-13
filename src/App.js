f2747f3282d8e2ae34ea0f4a30a9c2119433f4bf
mehdigoom <medhi.gay@sfr.fr>
2019-06-13 10:21:35 +0200

    save home page

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+
+class App extends React.Component {
+  constructor(props) {
+
+
+
+
+    
+    super(props);
+  
+    this.state = {
+titre : "Bienvenue Dans le support Dood",
+message : "j'ai besoin d'aide !",
+prossse : false,
+hidenhome:""
+
+    };
+
+    
+  };
+ 
+  render() {
+    
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   <h1>{this.state.titre}</h1>
+   <button>{this.state.message}</button>
 
+      </header>
+      <body>
+      
+      </body>
+   
+    </div>
+     );
+    }
+  }
 export default App;
+
