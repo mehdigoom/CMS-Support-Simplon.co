18b8476c8b9338cc676067dada16b2529fa2dd0a
mehdigoom <medhi.gay@sfr.fr>
2019-06-13 11:45:13 +0200

    Rendu Dynamique OK

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import FAQ from './etapes/FAQ.js'
 
 class App extends React.Component {
   constructor(props) {
@@ -16,9 +16,9 @@ class App extends React.Component {
 titre : "Bienvenue Dans le support Dood",
 message : "j'ai besoin d'aide !",
 prossse : false,
-step:0
-
-
+step:0,
+FAQ: <FAQ/>,
+rendre:""
     };
   
  
@@ -31,14 +31,18 @@ step:0
       this.setState({
         message : "retour",
     prossse: false,
-    step :0
+    step :0,
+    rendre: this.state.FAQ
     })
+   
     }else{
       this.setState({
         message : "j'ai besoin d'aide !",
       prossse: true,
-      step:1
+      step:1,
+      rendre: ""
     })
+    
     }  
   }
 
@@ -52,7 +56,8 @@ step:0
       <header className="App-header">
    <h1>{this.state.titre}</h1>
    <button onClick={this.handleClick.bind(this)}>{this.state.message}</button>
-
+  
+   {this.state.rendre}
       </header>
      
    
